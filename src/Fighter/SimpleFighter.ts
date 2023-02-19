483a842f4e49271e18607236f5f1ad79c7337392
Siigmaf <faby.tbe@gmail.com>
2023-02-19 13:51:34 -0300

    interface SimpleFighter

---
+++ src/Fighter/SimpleFighter.ts
@@ -0,0 +1,9 @@
+export interface SimpleFighter {
+  lifePoints:number;
+  strength:number;
+
+  attack(enemy:SimpleFighter):void;
+  receiveDamage(attackPoints:number):number;
+}
+
+export default SimpleFighter;
--- src/Fighter/index.ts
+++ src/Fighter/index.ts
@@ -1,3 +1,5 @@
 import IFighter from './Fighter';
+import { SimpleFighter } from './SimpleFighter';
 
 export default IFighter;
+export { SimpleFighter };
