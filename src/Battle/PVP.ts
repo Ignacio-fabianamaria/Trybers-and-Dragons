d4f606cabd743e0310eda2fc4703d5b640141bca
Siigmaf <faby.tbe@gmail.com>
2023-02-20 11:14:51 -0300

    implementa class PVP e edita class Battle

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -1,7 +1,26 @@
+import IFighter from '../Fighter';
 import Battle from './Battle';
 
 class PVP extends Battle {
+  constructor(
+    private _playerOne:IFighter,
+    private _playerTwo:IFighter,
+  ) {
+    super(_playerOne);
+    this._playerOne = _playerOne;
+    this._playerTwo = _playerTwo;
+  }
 
+  fight(): number {
+    // se os pontos de vida do playerOne e playerTwo forem acima de 0 eles podem duelar
+    if (this._playerOne.lifePoints > 0 && this._playerTwo.lifePoints > 0) {
+      this._playerOne.attack(this._playerTwo);// playerOne ataca playerTwo
+      this._playerTwo.attack(this._playerOne);// playerTwo ataca playerOne
+    }
+    return super.fight();// retorna o metodo fight da class Battle que possui as regras da batalha
+    // Se o retorno for 1, o player quer foi usado como parâmetro do super ganhou, 
+    // se retornar -1 a vitória fica com o player que não foi o parâmetro do super
+  }
 }
 
 export default PVP;
